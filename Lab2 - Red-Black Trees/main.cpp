705aa98e457919430260d871fae75c7d015ad7a1
Ilinca Bitir <ilincabitir24@gmail.com>
2026-02-28 20:29:58 +0200

    insertion+main

--- Lab2 - Red-Black Trees/main.cpp
+++ Lab2 - Red-Black Trees/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
-
-
 enum Color {red, black};
 
 class RBNode {
@@ -46,7 +43,6 @@ public:
         RBNode* y = x->right;
 
         x->right = y->left;
-
         if (y->left != RBNode::Nil)
             y->left->parent = x;
 
@@ -54,13 +50,12 @@ public:
 
         if (x->parent == RBNode::Nil)
             root=y;
-        else
-            if (x==x->parent->left)
+        else if (x==x->parent->left)
                 x->parent->left = y;
         else
             x->parent->right=y;
 
-        x->left=x;
+        y->left=x;
         x->parent = y;
     }
 
@@ -73,15 +68,121 @@ public:
         y->left = x->right;
         if (x->right != RBNode::Nil)
             x->right->parent =y;
+
         x->parent = y->parent;
+
         if (y->parent == RBNode::Nil)
             root=x;
-        else if (y=y->parent->left)
+        else if (y==y->parent->left)
             y->parent->left = x;
         else
             y->parent->right = x;
 
         x->right=y;
-        y->left=x;
+        y->parent=x;
+    }
+
+    //insertion:
+
+    void RBInsert(RBNode* z) {
+        RBNode* y= RBNode::Nil;
+        RBNode* x = root;
+
+        //normal BST insert
+        while (x!=RBNode::Nil) {
+            y=x;
+
+            if (z->key < x->key)
+                x=x->left;
+            else
+                x=x->right;
+        }
+
+        z->parent =y;
+        if (y==RBNode::Nil)
+            root=z;
+        else if (z->key < y->key)
+            y->left = z;
+        else
+            y->right = z;
+
+        //all leaves are black
+        z->left = RBNode::Nil;
+        z->right = RBNode::Nil;
+        z->color=red;  // make color of node red
+
+        RBInsertFixup(z);  //fix possible violations  (red node could have red parent -> we must fix that)
+    }
+
+    void RBInsertFixup(RBNode* z) {
+        while (z->parent->color == red) {
+            if (z->parent == z->parent->parent->left) {
+                RBNode* y=z->parent->parent->right;
+
+                if (y->color==red) {          //case 1
+                    z->parent->color=black;
+                    y->color=black;
+                    z->parent->parent->color = red;
+                    z=z->parent->parent;
+                }
+                else {
+                    if (z==z->parent->right) {       //case 2
+                        z=z->parent;
+                        LeftRotate(z);
+                    }
+                    z->parent->color=black;         //case 3
+                    z->parent->parent->color=red;
+                    RightRotate(z->parent->parent);
+                }
+            }
+            else{
+                //mirror cases
+                RBNode* y= z->parent->parent->left;
+                if (y->color==red) {
+                    z->parent->color=black;
+                    y->color=black;
+                    z->parent->parent->color=red;
+                    z=z->parent->parent;
+                }
+                else {
+                    if (z==z->parent->left) {
+                        z=z->parent;
+                        RightRotate(z);
+                    }
+
+                    z->parent->color=black;
+                    z->parent->parent->color=red;
+                    LeftRotate(z->parent->parent);
+                }
+            }
+        }
+        root->color=black;
+    }
+
+    void InorderPrint(RBNode* node) {
+        if (node == RBNode::Nil)
+            return;
+
+        InorderPrint(node->left);
+        cout << node->key << "(" << (node->color == red ? "R" : "B") << ") ";
+        InorderPrint(node->right);
     }
 };
+
+
+int main() {
+    RBTree tree;
+
+    int keys[] = {10, 20, 30, 15, 25, 5};
+    for (int k : keys) {
+        RBNode* node = new RBNode(k);
+        tree.RBInsert(node);
+    }
+
+    cout << "Inorder traversal:\n";
+    tree.InorderPrint(tree.root);
+    cout << endl;
+
+    return 0;
+}
+
