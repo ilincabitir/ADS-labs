c94dd9af4c4656000b71e231fde7087475bcbf16
Ilinca Bitir <ilincabitir24@gmail.com>
2026-02-28 20:47:05 +0200

    delete node

--- Lab2 - Red-Black Trees/main.cpp
+++ Lab2 - Red-Black Trees/main.cpp
@@ -167,21 +167,189 @@ public:
         cout << node->key << "(" << (node->color == red ? "R" : "B") << ") ";
         InorderPrint(node->right);
     }
+
+
+    RBNode* Successor(RBNode* z) {
+        RBNode* x = z->right;
+        while (x != RBNode::Nil) {
+            x = x->left;
+        }
+        return x;
+    }
+
+    RBNode* RBDelete(RBNode* z) {
+        RBNode* y;       // node to actually remove
+        RBNode* x;       // child that will replace y
+        Color yOriginalColor;
+
+        // find node to remove (y)
+        if (z->left == RBNode::Nil || z->right == RBNode::Nil)
+            y = z;                 // z has 0 or 1 child
+        else
+            y = Successor(z);      // z has 2 children, successor has at most 1 child
+
+        yOriginalColor = y->color;
+
+        // set x to y's child (or Nil if y is leaf)
+        if (y->left != RBNode::Nil)
+            x = y->left;
+        else
+            x = y->right;
+
+        // update x's parent pointer
+        x->parent = y->parent;
+
+        // splice out y from the tree
+        if (y->parent == RBNode::Nil)        // y was root
+            root = x;
+        else if (y == y->parent->left)       // y was left child
+            y->parent->left = x;
+        else                                 // y was right child
+            y->parent->right = x;
+
+        // if z had 2 children, copy y's key into z
+        if (y != z) {
+            z->key = y->key;
+
+        }
+
+        // fix R-B properties if a black node was removed
+        if (yOriginalColor == black)
+            RBDeleteFixup(x);
+
+        return y;
+    }
+
+
+    void RBDeleteFixup(RBNode* x) {
+    while (x != root && x->color == black) {
+        if (x == x->parent->left) {
+            RBNode* w = x->parent->right;
+
+            // case 1: sibling is red
+            if (w->color == red) {
+                w->color = black;
+                x->parent->color = red;
+                LeftRotate(x->parent);
+                w = x->parent->right;
+            }
+
+            // case 2: sibling black, both children are black
+            if (w->left->color == black && w->right->color == black) {
+                w->color = red;
+                x = x->parent;
+            }
+            else {
+                // case 3: sibling black, right child is black, left child is red
+                if (w->right->color == black) {
+                    w->left->color = black;
+                    w->color = red;
+                    RightRotate(w);
+                    w = x->parent->right;
+                }
+
+                // case 4: sibling is black, right child is red
+                w->color = x->parent->color;
+                x->parent->color = black;
+                w->right->color = black;
+                LeftRotate(x->parent);
+                x = root;
+            }
+        }
+        else { // mirror case
+            RBNode* w = x->parent->left;
+
+            if (w->color == red) {
+                w->color = black;
+                x->parent->color = red;
+                RightRotate(x->parent);
+                w = x->parent->left;
+            }
+
+            if (w->right->color == black && w->left->color == black) {
+                w->color = red;
+                x = x->parent;
+            }
+            else {
+                if (w->left->color == black) {
+                    w->right->color = black;
+                    w->color = red;
+                    LeftRotate(w);
+                    w = x->parent->left;
+                }
+
+                w->color = x->parent->color;
+                x->parent->color = black;
+                w->left->color = black;
+                RightRotate(x->parent);
+                x = root;
+            }
+        }
+    }
+    x->color = black;
+}
+
+    RBNode* Search(int key) {
+        RBNode* node = root;
+        while (node != RBNode::Nil) {
+            if (key == node->key)
+                return node;           // found the key
+            else if (key < node->key)
+                node = node->left;     // go left
+            else
+                node = node->right;    // go right
+        }
+        return RBNode::Nil;             // key not found
+    }
 };
 
+
+#include <iostream>
+using namespace std;
 
 int main() {
     RBTree tree;
 
-    int keys[] = {10, 20, 30, 15, 25, 5};
-    for (int k : keys) {
-        RBNode* node = new RBNode(k);
+    // --- Insertion ---
+    int n;
+    cout << "How many nodes do you want to insert? ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int key;
+        cout << "Enter key to insert: ";
+        cin >> key;
+
+        RBNode* node = new RBNode(key);
         tree.RBInsert(node);
+
+        cout << "Tree after inserting " << key << ":\n";
+        tree.InorderPrint(tree.root);
+        cout << endl;
     }
 
-    cout << "Inorder traversal:\n";
-    tree.InorderPrint(tree.root);
-    cout << endl;
+    // --- Deletion ---
+    cout << "\nNow you can delete nodes.\n";
+    char choice;
+    do {
+        int delKey;
+        cout << "Enter key to delete: ";
+        cin >> delKey;
+
+        // Use search function to find the node
+        RBNode* node = tree.Search(delKey);
+        if (node != RBNode::Nil) {
+            tree.RBDelete(node);
+            cout << "Tree after deleting " << delKey << ":\n";
+            tree.InorderPrint(tree.root);
+            cout << endl;
+        } else {
+            cout << "Node " << delKey << " not found in the tree.\n";
+        }
+
+        cout << "Delete another node? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
